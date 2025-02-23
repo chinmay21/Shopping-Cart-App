7448ff44feccb2371d95f7a527f52433d92b9c0a
chinmay21 <dhaundiyalchinmay@gmail.com>
2025-02-23 20:28:52 +0530

    Add responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,67 +1,35 @@
-import React, { useState } from "react";
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
   const { cart } = useSelector((state) => state);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div>
-      <nav className="flex justify-between items-center max-w-6xl mx-auto">
-        <NavLink to="/">
-          <div className="ml-5">
-            <img src={logo} width={100} height={100} alt="logo" />
-          </div>
+    <nav className="flex flex-wrap justify-between items-center max-w-6xl mx-auto p-4">
+      <NavLink to="/">
+        <div className="ml-2">
+          <img src={logo} width={100} height={100} alt="logo" />
+        </div>
+      </NavLink>
+
+      <div className="flex-grow flex justify-end space-x-6 font-medium text-slate-100">
+        <NavLink to="/" className="hover:text-gray-400 transition">
+          Home
         </NavLink>
 
-        <div className="hidden md:flex items-center font-medium text-slate-100 space-x-6">
-          <NavLink to="/">
-            <p>Home</p>
-          </NavLink>
-
-          <NavLink to="/cart">
-            <div className="relative">
-              <FaShoppingCart className="text-2xl" />
-              {cart.length > 0 && (
-                <span className="absolute -top-1 -right-2 bg-green-600 rounded-full text-sm w-5 h-5 flex justify-center items-center animate-bounce">
-                  {cart.length}
-                </span>
-              )}
-            </div>
-          </NavLink>
-        </div>
-
-        <div className="md:hidden flex items-center">
-          <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-slate-100">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-          </button>
-        </div>
-      </nav>
-
-      {isMenuOpen && (
-        <div className="md:hidden bg-gray-800 text-slate-100 py-4 space-y-4">
-          <NavLink to="/" onClick={() => setIsMenuOpen(false)} className="block text-center">
-            Home
-          </NavLink>
-
-          <NavLink to="/cart" onClick={() => setIsMenuOpen(false)} className="block text-center">
-            <div className="relative">
-              <FaShoppingCart className="text-2xl mx-auto" />
-              {cart.length > 0 && (
-                <span className="absolute -top-1 -right-2 bg-green-600 rounded-full text-sm w-5 h-5 flex justify-center items-center animate-bounce">
-                  {cart.length}
-                </span>
-              )}
-            </div>
-          </NavLink>
-        </div>
-      )}
-    </div>
+        <NavLink to="/cart" className="relative hover:text-gray-400 transition">
+          <FaShoppingCart className="text-2xl" />
+          {cart.length > 0 && (
+            <span className="absolute -top-1 -right-2 bg-green-600 rounded-full text-sm w-5 h-5 flex justify-center items-center animate-bounce">
+              {cart.length}
+            </span>
+          )}
+        </NavLink>
+      </div>
+    </nav>
   );
 };
 
